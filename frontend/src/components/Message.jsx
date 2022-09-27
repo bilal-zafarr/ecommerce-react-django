1f1cfde324632329cb1e2a8f91eb0cb0581372b0
Bilal Zafar <bilalzaafar@gmail.com>
2022-09-27 19:31:33 +0500

    redux implemented for state management

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,11 @@
+import { Alert } from "react-bootstrap"
+
+const Message = ({ children, variant }) => {
+    return (
+        <Alert variant={variant}>
+            {children}
+        </Alert>
+    )
+}
+
+export default Message
