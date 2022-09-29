a26991784df991402394c5e052c3e921ab79db63
Bilal Zafar <bilalzaafar@gmail.com>
2022-09-29 21:20:39 +0500

    cart functionality implemented

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`);
@@ -17,6 +17,10 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 }
 
-// Language: javascript
-// Path: frontend/src/actions/productActions.js
-// Compare this snippet from frontend/src/store.js:
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
+    });
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+}
