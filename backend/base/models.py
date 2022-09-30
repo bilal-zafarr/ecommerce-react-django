a572a0b7d30a8f708f9c58ba10a79ffff59610a7
Bilal Zafar <bilalzaafar@gmail.com>
2022-09-30 20:34:38 +0500

    implementing jwts at backend

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,5 +1,3 @@
-from ctypes import addressof
-
 from django.contrib.auth.models import User
 from django.db import models
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,14 +1,60 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import *
 from .products import products
 from .serializer import *
 
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    # if you want to add data in jwt itself then you can use this method
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+
+    #     # Add custom claims
+    #     token["username"] = user.username
+    #     token["message"] = "hello jwt"
+    #     # ...
+
+    #     return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data["username"] = self.user.username
+        # data["email"] = self.user.email
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(["GET"])
 def getRoutes(request):
-    return Response("Here are your routes")
+    routes = [
+        "/api/products",
+        "/api/products/:id",
+        "/api/products/create",
+    ]
+    return Response(routes)
+
+
+@api_view(["GET"])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
